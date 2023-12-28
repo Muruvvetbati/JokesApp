61aca7b0ceaccd7e76b06a97eec1a3232d9f7e90
Muruvvetbati <batimuruvvet@gmail.com>
2023-12-28 22:34:26 +0300

    Initial Commit

---
+++ JokeSwiftUI/JokeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeSwiftUIApp.swift
+//  JokeSwiftUI
+//
+//  Created by Devmuruvvet on 28.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct JokeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
